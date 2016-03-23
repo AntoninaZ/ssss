ab83ec051311ada9e471d2efc1631a888135068d
antonina.zhuk@thinkmobiles.com <aiphei7Sieku>
2016-03-23 23:40:48 +0200

    //

--- app/src/main/java/game/snake/ua/MainActivityFragment.java
+++ app/src/main/java/game/snake/ua/MainActivityFragment.java
@@ -9,6 +9,8 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -39,10 +41,8 @@ public class MainActivityFragment extends Fragment implements View.OnClickListen
         textViewGameField.setText("Please, touch to screen for start game");
       /*  DisplayMetrics metrics = new DisplayMetrics();
         getActivity().getWindowManager().getDefaultDisplay().getMetrics(metrics);
-
         widthGameField = metrics.widthPixels;
         heightGameField = metrics.heightPixels;
-
         float c = textViewGameField.getPaint().measureText(" ");
         countSymbolOfLine = (int) (widthGameField / c);*/
 
@@ -53,20 +53,17 @@ public class MainActivityFragment extends Fragment implements View.OnClickListen
 
     @Override
     public void onStart() {
-         super.onStart();
+        super.onStart();
     }
 
     private void createGameField() {
         //***create array game field***
         strArrGameField = new String[countLine][countSymbolOfLine];
         //***draw game field***
-         for (int i = 0; i < countLine; i++) {
+        for (int i = 0; i < countLine; i++) {
             for (int j = 0; j < countSymbolOfLine; j++) {
-                strArrGameField[i][j] = "/";
-                if (( i == 0 ) || ( j == 0) || ( i == countLine - 1 ) || ( j == countSymbolOfLine - 1 )) {
-                    strArrGameField[i][j] = "#";
-                }
-
+                strArrGameField[i][j] = ".";
+                strArrGameField[0][j] = "#";
             }
         }
         //***determine the position of the snake's head***
@@ -108,9 +105,29 @@ public class MainActivityFragment extends Fragment implements View.OnClickListen
         textViewGameField.setText(strField);
     }
 
-    private void moveOfSnake(int x, int y) throws InterruptedException {
-        int k = 1;
-        switch (directBody) {
+    private void moveOfSnake(final int x, final int y) {
+
+            Timer t = new Timer(false);
+            t.schedule(new TimerTask() {
+
+                @Override
+                public void run() {
+                    int k = 0;
+                    while (!((x + k) == (countLine - 3))) {
+                    strArrGameField[x + k - 2][y] = ".";
+                    strArrGameField[x + k][y] = "@";
+                    getActivity().runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            visibleGameField();
+                        }
+                    });k++;
+                }}
+            }, 6000);
+
+
+
+       /* switch (directBody) {
             case 0:
                 while (!(x == countLine - 1 || y == countSymbolOfLine ||  x == 0 || y == 0)) {
                     x ++;
@@ -152,7 +169,7 @@ public class MainActivityFragment extends Fragment implements View.OnClickListen
                     Thread.sleep(1000);
                 }
                 break;
-        }
+        }*/
     }
 
     @Override
@@ -173,15 +190,13 @@ public class MainActivityFragment extends Fragment implements View.OnClickListen
         countSymbolOfLine = (int) (widthGameField / textViewGameField.getPaint().measureText(" "));
         countLine = heightGameField / textViewGameField.getLineHeight();
 
+
         createGameField();
         visibleGameField();
         int posHeadX = countLine / 2;
         int posHeadY = countSymbolOfLine / 2;
-        try {
-            moveOfSnake(posHeadX, posHeadY);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        moveOfSnake(posHeadX, posHeadY);
+
         return false;
     }
 }
