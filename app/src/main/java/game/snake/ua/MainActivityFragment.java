97ddde72fd88181f83cbd06c6f96f9454a0b5f7f
antonina.zhuk@thinkmobiles.com <aiphei7Sieku>
2016-03-27 23:54:01 +0300

    //

--- app/src/main/java/game/snake/ua/MainActivityFragment.java
+++ app/src/main/java/game/snake/ua/MainActivityFragment.java
@@ -2,18 +2,20 @@ package game.snake.ua;
 
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 /**
  * A placeholder fragment containing a simple view.
  */
-public class MainActivityFragment extends Fragment implements View.OnClickListener, View.OnTouchListener {
+public class MainActivityFragment extends Fragment implements View.OnTouchListener {
     private TextView textViewGameField;
 
     private int countSymbolOfLine;
@@ -21,13 +23,15 @@ public class MainActivityFragment extends Fragment implements View.OnClickListen
     private int directBody;
 
     private String[][] strArrGameField;
+    private ArrayList<Position> snakeMap;
 
-    String strField = " ";
     Integer k;
-    boolean isRunning  = false;
-    boolean isFirstTouch = true;
-    public MainActivityFragment() {
-    }
+
+    private boolean isRunning  = false;
+    private boolean isFirstTouch = true;
+
+    private int posHeadX;
+    private int posHeadY;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -52,101 +56,147 @@ public class MainActivityFragment extends Fragment implements View.OnClickListen
     private void createGameField() {
         //***create array game field***
         strArrGameField = new String[countLine][countSymbolOfLine];
+
+
         //***draw game field***
         for (int i = 0; i < countLine; i++) {
             for (int j = 0; j < countSymbolOfLine; j++) {
-                strArrGameField[i][j] = ".";
+                strArrGameField[i][j] = "&#160;";
                 strArrGameField[0][j] = "#";
             }
         }
-        //***determine the position of the snake's head***
-        int posHeadX = countLine / 2;
-        int posHeadY = countSymbolOfLine / 2;
+    }
+
+    private void createSnake() {
+        //***create snake
+        snakeMap = new ArrayList();
+
+        //***determine the position of the snake's head***//
+        posHeadX = countLine / 2;
+        posHeadY = countSymbolOfLine / 2;
+
+        Position position = new Position();
+        position.setPos(posHeadX, posHeadY);
+        snakeMap.add(position);
 
         //***determine the position of the snake's body***
         directBody = new Random().nextInt(4);
-        strArrGameField[posHeadX][posHeadY] = "@";
+;
         switch (directBody) {
             case 0:
-                strArrGameField[posHeadX+1][posHeadY] = "@";
-                strArrGameField[posHeadX+2][posHeadY] = "@";
+                position.setPos(posHeadX + 1, posHeadY);
+                snakeMap.add(position);
+                position.setPos(posHeadX + 2, posHeadY);
+                snakeMap.add(position);
                 break;
             case 1:
-                strArrGameField[posHeadX][posHeadY+1] = "@";
-                strArrGameField[posHeadX][posHeadY+2] = "@";
+                position.setPos(posHeadX, posHeadY + 1);
+                snakeMap.add(position);
+                position.setPos(posHeadX, posHeadY + 2);
+                snakeMap.add(position);
                 break;
             case 2:
-                strArrGameField[posHeadX-1][posHeadY] = "@";
-                strArrGameField[posHeadX-2][posHeadY] = "@";
+                position.setPos(posHeadX - 1, posHeadY);
+                snakeMap.add(position);
+                position.setPos(posHeadX - 2, posHeadY);
+                snakeMap.add(position);
                 break;
             case 3:
-                strArrGameField[posHeadX][posHeadY-1] = "@";
-                strArrGameField[posHeadX][posHeadY-2] = "@";
+                position.setPos(posHeadX, posHeadY - 1);
+                snakeMap.add(position);
+                position.setPos(posHeadX, posHeadY - 2);
+                snakeMap.add(position);
                 break;
         }
+        strArrGameField[snakeMap.get(0).getPosX()][snakeMap.get(0).getPosY()] = "@";
+        strArrGameField[snakeMap.get(1).getPosX()][snakeMap.get(1).getPosY()] = "@";
+        strArrGameField[snakeMap.get(2).getPosX()][snakeMap.get(2).getPosY()] = "@";
     }
 
     private void visibleGameField() {
-        strField = "";
+        String strField = "";
         for (int i = 0; i < countLine; i++) {
             for (int j = 0; j < countSymbolOfLine; j++) {
                 strField += strArrGameField[i][j];
             }
         }
-        textViewGameField.setText(strField);
+        textViewGameField.setText(Html.fromHtml(strField));
     }
 
     //***cX - current x
     //***cY - current y
-    //***dX - destination x
-    //***dY - destination y
-    private boolean moveSnake(int cX, int cY, int dX, int dY ) {
+
+    private boolean moveSnake() {
+        Position position = new Position();
         boolean runResult = false;
         switch (directBody) {
-
-            case 0:
-                if ((cX + k) < (countLine - 2)) {
-                    strArrGameField[cX + k - 1][cY] = ".";
-                    strArrGameField[cX + 2 + k][cY] = "@";
-                    visibleGameField();
-                    k++;
+            case 0://***Direction down***//
+                if (snakeMap.get(snakeMap.size() - 1).getPosX() < countLine - 1) {
+                    position.setPosX(snakeMap.get(snakeMap.size() - 1).getPosX() + 1);
+                    position.setPosY(snakeMap.get(snakeMap.size() - 1).getPosY());
+                    // strArrGameField[cX + 2 + k][cY] = "@";
+                   // strArrGameField[cX + k - 1][cY] = ".";
+                    snakeMap.add(position);
+                    snakeMap.remove(0);
                     runResult = true;
                 } else runResult = false;
                 break;
-            case 1:
-                if ((cY + k) < (countSymbolOfLine - 2)) {
-                    strArrGameField[cX][cY + k - 1] = ".";
-                    strArrGameField[cX][cY + 2 + k] = "@";
-                    visibleGameField();
-                    k++;
+            case 1://***Direction right***//
+                if (snakeMap.get(snakeMap.size() - 1).getPosY() < countSymbolOfLine - 1) {
+                    position.setPosX(snakeMap.get(snakeMap.size() - 1).getPosX());
+                    position.setPosY(snakeMap.get(snakeMap.size() - 1).getPosY() + 1);
+                    //strArrGameField[cX][cY + 2 + k]= "@";
+                    //strArrGameField[cX][cY + k - 1] = ".";
+                    snakeMap.add(position);
+                    snakeMap.remove(0);
                     runResult = true;
                 } else runResult = false;
                 break;
-            case 2:
-                if ((cX - k) > 3) {
-                    strArrGameField[cX - k + 1][cY] = ".";
-                    strArrGameField[cX - 2 - k][cY] = "@";
-                    visibleGameField();
-                    k++;
+            case 2://***Direction top***//
+                if (snakeMap.get(0).getPosX() > 1) {
+                    position.setPosX(snakeMap.get(0).getPosX() - 1);
+                    position.setPosX(snakeMap.get(0).getPosY());
+                    //strArrGameField[cX - 2 - k][cY] = "@";
+                    //strArrGameField[cX - k + 1][cY] = ".";
+                    snakeMap.add(position);
+                    snakeMap.remove(snakeMap.size() - 1);
                     runResult = true;
                 } else runResult = false;
                 break;
+            //***Direction left***//
             case 3:
-                if ((cY - k) > 3) {
-                    strArrGameField[cX][cY - k + 1] = ".";
-                    strArrGameField[cX][cY - 2 - k] = "@";
-                    visibleGameField();
-                    k++;
+                if (snakeMap.get(0).getPosY() > 1) {
+                    position.setPosX(snakeMap.get(0).getPosX());
+                    position.setPosX(snakeMap.get(0).getPosY() - 1);
+                    //strArrGameField[cX][cY - 2 - k] = "@";
+                    // strArrGameField[cX][cY - k + 1] = ".";
+                    snakeMap.add(position);
+                    snakeMap.remove(snakeMap.size() - 1);
                     runResult = true;
                 } else runResult = false;
                 break;
         }
+        strArrGameField[snakeMap.get(0).getPosX()][snakeMap.get(0).getPosY()] = "@";
+        strArrGameField[snakeMap.get(1).getPosX()][snakeMap.get(1).getPosY()] = "@";
+        strArrGameField[snakeMap.get(2).getPosX()][snakeMap.get(2).getPosY()] = "@";
         return runResult;
     }
+    //xT - x from touch
+    //yT - y from touch
+    private void defineDirection(int xT, int yT) {
+        switch (directBody) {
+            case 0:case 2:
+                if (yT < snakeMap.get(snakeMap.size() - 1).getPosY()) {
+                    directBody = 1;
+                } else directBody = 3;
+                break;
 
-    @Override
-    public void onClick(View view) {
-
+            case 1:case 3:
+                if (xT < snakeMap.get(snakeMap.size() - 1).getPosX()) {
+                    directBody = 0;
+                } else directBody = 2;
+                break;
+        }
     }
 
     @Override
@@ -154,11 +204,14 @@ public class MainActivityFragment extends Fragment implements View.OnClickListen
         if (isFirstTouch) {
             getSizeGameField();
             createGameField();
+            createSnake();
             visibleGameField();
             isFirstTouch = false;
-
-            final int x = countLine / 2;
-            final int y = countSymbolOfLine / 2;
+        } else {
+            int xDist = (int) (event.getY() / textViewGameField.getLineHeight());
+            int yDist = (int) (event.getX() / textViewGameField.getPaint().measureText(" "));
+            defineDirection(xDist, yDist);
+        }
 
             (new Thread(new Runnable() {
                 @Override
@@ -167,13 +220,15 @@ public class MainActivityFragment extends Fragment implements View.OnClickListen
                     isRunning = true;
                     while (!Thread.interrupted())
                         try {
-                            Thread.sleep(500);
+                            Thread.sleep(1000);
                             getActivity().runOnUiThread(new Runnable() {
 
                                 @Override
                                 public void run() {
                                     if (isRunning) {
-                                        isRunning = moveSnake(x, y, x, x);
+                                        createGameField();
+                                        isRunning = moveSnake();
+                                        visibleGameField();
                                     } else Thread.interrupted();
                                 }
                             });
@@ -182,10 +237,7 @@ public class MainActivityFragment extends Fragment implements View.OnClickListen
                         }
                 }
             })).start();
-        } else {
 
-
-        }
         return false;
     }
 }
---
+++ app/src/main/java/game/snake/ua/Position.java
@@ -0,0 +1,30 @@
+package game.snake.ua;
+
+/**
+ * Created by antonina on 27.03.16.
+ */
+public class Position {
+    private int posX;
+    private int posY;
+
+    public int getPosX() {
+        return posX;
+    }
+
+    public int getPosY() {
+        return posY;
+    }
+
+    public void setPosX(int posX) {
+        this.posX = posX;
+    }
+
+    public void setPosY(int posY) {
+        this.posY = posY;
+    }
+
+    public void setPos(int posX, int posY) {
+        this.posX = posX;
+        this.posY = posY;
+    }
+}
